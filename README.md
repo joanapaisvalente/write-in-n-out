ed341494b7f0df217995d7b194edad8f66cbf63c
Joana Valente <108897611+joanapaisvalente@users.noreply.github.com>
2023-09-18 19:27:33 +0100

    read me, text color change and text changing

---
+++ README.md
@@ -0,0 +1,9 @@
+## Why did I do this?
+I was just really bored one day scrolling and saw an animation for this write in and out text, I looked at it and was like 'this is never gonna work', and guess what? I was right.
+
+This made me really annoyed for some reason so I tried to make it work and here you have it.
+
+Btw I looked at a code pen (https://codepen.io/theking04123/pen/MWKVXxy) for inspiration. At first, I just wanted to make it work with a simple phrase (as seen on the code pen) and improve a bit, but then I thought that it would be more fun if the phrase changed, and that's what we have now folks.
+
+## But how can I run this?
+On VSCode, install Live Server extension. After you cloned this just click Go Live on the bottom right corner.
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const text = document.querySelector(".text");
 
-const typedText = "I'm a Developer.";
+const textArray = [
+    "I'm a Developer.", 
+    "I love programming.", 
+    "Live, Laugh, Code."
+];
 
-const typedTextArray = typedText.split('');
+let textPosition = 0;
+let typedTextArray = textArray[textPosition].split('');
 
 function getRandomTime() {
     return Math.random() * 0.55;
+}
+
+function changeTypedTextArray() {
+    textPosition === textArray.length - 1 ? textPosition = 0 : textPosition++;
+    typedTextArray = textArray[textPosition].split('');
 }
 
 function writeText(currentLetter = 0) {
@@ -13,7 +23,6 @@ function writeText(currentLetter = 0) {
     setTimeout(() => {
         const currentText = text.innerHTML;
         const finalText = currentText + typedTextArray[currentLetter];
-        console.log(finalText)
         
         text.innerHTML = finalText;
 
@@ -29,10 +38,13 @@ function deleteText() {
     setTimeout(() => {
         const currentText = text.innerHTML;
         const finalText = currentText.slice(0, -1);
+
         text.innerHTML = finalText;
+
         let finalTextLength = finalText.split('').length;
 
         if (finalTextLength === 0) {
+            changeTypedTextArray();
             return writeText(0);
         }
         return deleteText(--finalTextLength);
